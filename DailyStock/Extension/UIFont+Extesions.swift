257dd350acc43d75df17bd617ab09b01e7c18a54
jmkim <wjdals0304@gmail.com>
2022-03-13 15:56:40 +0900

    add : 폰트 추가

--- DailyStock/Extension/UIFont+Extesions.swift
+++ DailyStock/Extension/UIFont+Extesions.swift
@@ -11,13 +11,28 @@ enum FontSize {
     
     case Bold_15
     
+    case Bold_16
+    
+    case Bold_18
+    
     case Bold_20
 
+    case Bold_22
+    
     case Bold_24
     
     case Bold_25
+    
+    case Bold_28
 
     case Bold_30
+    
+    case regular_18
+
+    case regular_20
+    
+    case regular_24
+    
     
 }
 
@@ -28,17 +43,34 @@ extension UIFont {
         
         switch size {
             
+        
+            
+            
         case .Bold_15 :
             return UIFont(name: "Roboto-Bold", size: 15)!
+        case .Bold_16 :
+            return UIFont(name: "Roboto-Bold", size: 16)!
+        case .Bold_18 :
+            return UIFont(name: "Roboto-Bold", size: 18)!
         case .Bold_20 :
             return UIFont(name: "Roboto-Bold", size: 20)!
+        case .Bold_22:
+            return UIFont(name: "Roboto-Bold", size: 22)!
         case .Bold_24 :
             return UIFont(name: "Roboto-Bold", size: 24)!
         case .Bold_25 :
             return UIFont(name: "Roboto-Bold", size: 25)!
+        case .Bold_28:
+            return UIFont(name: "Roboto-Bold", size: 28)!
         case .Bold_30 :
             return UIFont(name: "Roboto-Bold", size: 30)!
-            
+        case .regular_18:
+            return UIFont(name: "Roboto-Regular", size: 18)!
+        case .regular_20:
+            return UIFont(name: "Roboto-Regular", size: 20)!
+        case .regular_24:
+            return UIFont(name: "Roboto-Regular", size: 24)!
+
         }
     
     }
