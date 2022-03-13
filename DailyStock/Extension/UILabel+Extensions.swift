7f1e60a45b3a7594e878b072c5ee846437257fd0
jmkim <wjdals0304@gmail.com>
2022-03-13 15:57:12 +0900

    add: 특정 폰트 볼드로 변환 함수 추가

--- DailyStock/Extension/UILabel+Extensions.swift
+++ DailyStock/Extension/UILabel+Extensions.swift
@@ -14,16 +14,23 @@ extension UILabel {
     func KeywordBlue(_ keyword: String) {
       
       let fullText = self.text ?? ""
-      
       let range = (fullText as NSString).range(of: keyword)
-      
       let textColor = UIColor.systemBlue
-      
       let attributedString = NSMutableAttributedString(string: fullText)
-      
       attributedString.addAttribute(.foregroundColor, value: textColor, range: range)
-      
-        
       self.attributedText = attributedString
+        
+    }
+    
+    func asFont(targetString: String) {
+        
+        let fullText = text ?? ""
+        let attributedString = NSMutableAttributedString(string: fullText)
+        let font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        
+        let range = (fullText as NSString).range(of: targetString)
+        attributedString.addAttribute(.font, value: font, range: range)
+        attributedText = attributedString
+        
     }
 }
